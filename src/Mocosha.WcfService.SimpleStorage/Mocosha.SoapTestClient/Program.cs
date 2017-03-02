f3a0b3eee6bdd77e6ed454c42d3bd46ae5cf46af
Bojan Pantovic <bojan.pantovic@limolabs.com>
2017-03-02 13:33:34 +0100

    Web services solution added

---
+++ src/Mocosha.WcfService.SimpleStorage/Mocosha.SoapTestClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mocosha.SoapTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoapService.Service1Client ss = new SoapService.Service1Client();
+            var result = ss.GetData(5);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
+        }
+    }
+}
