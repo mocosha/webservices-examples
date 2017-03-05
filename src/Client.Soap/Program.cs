03e1de6f0a6efba7a6515db54dedc22012694c8e
Branimir <branimir.momcilovic@gmail.com>
2017-03-05 04:45:03 -0500

    Projects renamed

--- src/Client.Soap/Program.cs
+++ src/Client.Soap/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mocosha.SoapTestClient
 {
